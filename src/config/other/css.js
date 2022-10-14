887d6b05f15c1f597d989921e0a2416483cd2c27
743112096@qq.com <743112096@qq.com>
2022-10-14 17:58:04 +0800

    初始化

---
+++ src/config/other/css.js
@@ -0,0 +1,12 @@
+/**
+ * @created by kongqf
+ * @description 样式导入
+ */
+import store from '@/store';
+// import '@/assets/styles/element-variables.scss'
+// import '@/assets/styles/index.scss' // global css
+// import 'yuwp-ui/lib/theme-default/index.css'; // global css
+// import '@/assets/common/zrcbankicon/iconfont.css';
+// import '@/assets/common/iconfont/iconfont.css';
+// import '@/assets/common/icoFonts/icoFonts.css';
+import '@/assets/common/common.css'; // 基础样式
