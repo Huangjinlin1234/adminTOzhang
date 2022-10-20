fc6ac6d469a057c2add22471298b061dfe29f7e6
743112096@qq.com <743112096@qq.com>
2022-10-20 11:10:16 +0800

    业务组件封装

--- src/views/pages/console/common/selectTree.vue
+++ src/views/pages/console/common/selectTree.vue
@@ -1,8 +1,7 @@
 <template>
   <el-select v-model="value" placeholder="请选择">
-    <template>
-      ddd
-    </template>
+    <el-option hidden />
+    <el-tree :data="data" :props="defaultProps" />
   </el-select>
 </template>
 
@@ -10,7 +9,46 @@
 export default {
   data() {
     return {
-      value: ''
+      value: '',
+      data: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
+          }]
+        }, {
+          label: '二级 2-2',
+          children: [{
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
+          }]
+        }, {
+          label: '二级 3-2',
+          children: [{
+            label: '三级 3-2-1'
+          }]
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
     };
   }
 };
