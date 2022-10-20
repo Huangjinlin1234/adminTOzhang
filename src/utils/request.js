5c447391956f22ba009a0017953b21486b777d33
lijl3310650059 <3310650059@qq.com>
2022-10-20 15:20:43 +0800

    fix:删除多余请求头字段

--- src/utils/request.js
+++ src/utils/request.js
@@ -23,8 +23,9 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers["X-Token"] = getToken();
+      // config.headers["X-Token"] = getToken();
     }
+    // config.headers["Content-Type"] = "text/plain";
     return config;
   },
   error => {
@@ -48,7 +49,7 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data;
-
+    console.log("response", response);
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== "0") {
       Message({
